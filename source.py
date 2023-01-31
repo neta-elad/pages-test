73f1f514516e2055af03b5d7fdbc0888f53d0984
Neta Elad <elad.neta@gmail.com>
2023-01-31 21:40:06 +0200

    dynamic

--- source.py
+++ source.py
@@ -1,1 +1,10 @@
 print("Hello from source.py")
+
+# importing datetime module for now()
+import datetime
+ 
+# using now() to get current time
+current_time = datetime.datetime.now()
+ 
+# Printing value of now.
+print("Time now at greenwich meridian is:", current_time)
